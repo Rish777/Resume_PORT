68cccf8fb2c704dd17d8145e1b55112351efd554
Rish777 <mailtoriash@gmail.com>
2025-05-24 23:05:43 +0530

    dasdas

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,18 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build', // Specifies the output directory
+    chunkSizeWarningLimit: 1000, // Sets the warning threshold to 1000 kB
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Example: Separate vendor libraries into their own chunk
+          vendor: ['react', 'react-dom']
+        }
+      }
+    }
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
