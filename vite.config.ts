d44a062ef893007ee3501df3c7002d6b36ee6708
Rishabh Shukla <56560637+Rish777@users.noreply.github.com>
2026-04-26 19:03:36 +0530

    git executable

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Resume_PORT/',
   build: {
     outDir: 'build', // Specifies the output directory
     chunkSizeWarningLimit: 1000, // Sets the warning threshold to 1000 kB
